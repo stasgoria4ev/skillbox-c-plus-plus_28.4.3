9aa2259299a39445c84df5a152d5fcdc2da08b2a
Stanislav Goria4ev <stas.goria4ev@mail.ru>
2023-01-05 21:05:17 +0400

    сorrection of remarks

--- myprogram.cpp
+++ myprogram.cpp
@@ -10,38 +10,35 @@ std::vector <Dish> dishes;
 std::mutex scoreboard;
 
 void Courier(Restaurant* restaurant) {
-    for (;;) {
+    while (restaurant->deliveriesCount < 10) {
         std::this_thread::sleep_for(std::chrono::seconds(30));
-            
+        
+        scoreboard.lock();       
         if (dishes.front().ready) {
-            
-            scoreboard.lock(); 
             std::cout << dishes.front().name << " dish delivery." << '\n';
-            scoreboard.unlock();
-                
             dishes.erase(dishes.begin());
             restaurant->deliveriesCount++;
         }
+        scoreboard.unlock();
     }
 }
 
-void Waiting() {
+void Waiting(Restaurant* restaurant) {
     std::srand(std::time(nullptr));
 
-    while (true) {
+    while (restaurant->deliveriesCount < 10) {
         std::this_thread::sleep_for(std::chrono::seconds((rand() % 11 + 5)));
         
         bool found = false;
+        
+        scoreboard.lock();
         for(int i = 0; i < dishes.size() && !found; i++) 
             if (!dishes[i].ready) {
                 dishes[i].ready = true;
-
-                scoreboard.lock();
                 std::cout << "Dish " << dishes[i].name << " ready.\n";
-                scoreboard.unlock();
-
                 found = true;
             } 
+        scoreboard.unlock();
     }
 }
 
@@ -50,21 +47,16 @@ int main() {
     
     Restaurant* restaurant = new Restaurant;
     
-    //-----------курьер------------------------
-    std::thread delivery(Courier, restaurant);
+    std::thread cooking(Waiting, restaurant);
+    std::thread delivery(Courier, restaurant);                                   
+    cooking.detach();                                                           
     delivery.detach();
-    //-----------курьер------------------------
-
-    //-----------кухня-------------------------
-    std::thread cooking(Waiting);
-    cooking.detach(); 
-    //-----------кухня-------------------------
 
     while (restaurant->deliveriesCount < 10) {
         Dish dish;
-        dishes.push_back(dish);
         
         scoreboard.lock();
+        dishes.push_back(dish);
         std::cout << "Online order " << dishes.back().name << " dish." << '\n'; 
         scoreboard.unlock();
         
